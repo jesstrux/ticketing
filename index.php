1ae082816399b45ebb9f4633ecfe2ce5339c283f
Walter Kimaro <wakyj07@gmail.com>
2017-07-04 14:46:21 +0100

    Fix seat number bug

--- index.php
+++ index.php
@@ -14,7 +14,18 @@
 ?>	
 
 <div style="max-width: 85%; margin: 30px auto;">
-	<?php require_once "includes/templates/home_for_usertype_$user->role.php"; ?>
+	<?php
+		if (isset($_GET['ticket_creation'])) {
+			$created = $_GET['ticket_creation'] == 1 ? true : false;
+			$alert_type= !$created ? "error" : "success";
+			$dismiss_link="index.php";
+			$alert_message = !$created ? "<strong>Error!</strong> " : "<strong>Success!</strong> ";
+			$alert_message .= $created ? "Ticket successfully created." : "Sorry, ticket couldn't be created.";
+
+			include 'includes/templates/alert.php';
+		}
+		require_once "includes/templates/home_for_usertype_$user->role.php";
+	?>
 </div>
 
 <?php include('includes/templates/footer.php'); ?>
