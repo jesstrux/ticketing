bedb994300f5aee12c45a380de6c8429dbbfa437
Walter Kimaro <wakyj07@gmail.com>
2017-07-09 20:38:08 +0300

    Add choosing picture option during register

--- register.php
+++ register.php
@@ -9,13 +9,30 @@
 	<?php
 		if(isset($_POST['submit'])){
 			require_once 'includes/User.php';
+			require_once 'includes/FileUploader.php';
 
 			$user = new User();
 			$user->name = $_POST['name'];
 			$user->username = $_POST['username'];
 			$user->password = md5($_POST['password']);
 			$user->role = $_POST['role'];
-			$user->user_photo = "default_dp.png";
+
+			$dir_path = "assets/images/profile_pictures/";
+			$dir_path .= $_POST['role'] == 1 ? "buses/" : "";
+
+			if(isset($_FILES['user_photo']) && $_FILES['user_photo'] != null){
+				$fileUploader = new FileUploader("user_photo", $dir_path);
+				$upload_file = json_decode($fileUploader->upload());
+
+				if($upload_file->success){
+					$user->user_photo = $_POST['role'] == 1 ? "buses/" : "";
+					$user->user_photo .= $upload_file->target_file;
+				}
+				else
+					$user->user_photo = "default_dp.png";
+			}
+			else
+				$user->user_photo = "default_dp.png";
 
 			if(!$user->exists("username = '". $_POST['username']."'")){
 				if($user->create()){
@@ -38,7 +55,7 @@
 			$alert_message = "<strong>Success!</strong> Registration failed.";
 		}
 	?>
-	<form action="register.php" method="POST" style="padding-bottom: 5px;">
+	<form action="register.php" method="POST" style="padding-bottom: 5px;" enctype="multipart/form-data">
 		<div class="round-bg"></div>
 		<a class="link" href="login.php">LOGIN</a>
 		<div class="form-header">
@@ -63,11 +80,15 @@
 		<div class="input-wrapper">
 			<span>Register As: </span>
 			<select name="role" id="role">
-				<option value="">Choose option</option>
+				<option value="0">Choose option</option>
 				<option value="0">User</option>
 				<option value="1">Bus Owner</option>
 			</select>
 		</div>
+		<div class="input-wrapper">
+			<span>Picture:</span> <input id="user_photo" name="user_photo" type="file">
+		</div>
+
 		<div style="margin-botto: -25px;">&nbsp;</div>
 		<input type="submit" name="submit" value="Register Now">&emsp;
 		<!-- <button type="reset">Clear Form</button> -->
